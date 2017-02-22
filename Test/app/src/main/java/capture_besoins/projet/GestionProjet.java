bb499ac4e97e9e40a56a5c1e3d4a9d1c6b1cb160
Karl Ferrero <karl.ferrero.kf@gmail.com>
2017-02-22 17:03:37 +0100

    #2 Liste des projets qui se remplit avec les projets existants

--- Test/app/src/main/java/capture_besoins/projet/GestionProjet.java
+++ Test/app/src/main/java/capture_besoins/projet/GestionProjet.java
@@ -1,8 +1,8 @@
 package capture_besoins.projet;
 
 import android.app.Activity;
-import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -18,25 +18,23 @@ import capture_besoins.main.R;
 
 public class GestionProjet implements View.OnClickListener {
 
-    /** Activité principale */
-    Activity activity;
+    //Activité principale
+    private Activity activity;
 
-    /** Bouton pour créer un projet */
-    Button btn_creerProjet;
+    //Bouton pour créer un projet
+    private Button btn_creerProjet;
 
-    /**  Liste des projets */
-    ListView list_projets;
+    // Liste des projets
+    private ListView list_projets;
 
-    //private static final String repName = "Capture_Besoins";
+    // Variable temporaire du nom/id utilisateur
+    private final String varTempoNomUser = "userTest";
 
 
     public GestionProjet(Activity activity) {
 
         // On récupère l'activity
         this.activity = activity;
-
-        // TODO Chercher dans les fichiers
-        List<File> listProjets = getProjets();
 
         // On récupére le bouton pour créer un projet
         btn_creerProjet = (Button) activity.findViewById(R.id.btn_createProjet);
@@ -47,20 +45,55 @@ public class GestionProjet implements View.OnClickListener {
         // On récupère le ListView des projets
         list_projets = (ListView) activity.findViewById(R.id.listProjets);
 
+        // On remplit la liste des projets
+        remplirListeProjets();
+
+
+    }
+
+    private void remplirListeProjets() {
+        // On récupère la liste des Projets déja créés
+        List<File> listProjets = getProjets();
+
+        List<String> listNomProjets = new ArrayList<>();
+
+        for (File projet : listProjets) {
+            listNomProjets.add(projet.getName());
+        }
+
+        //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
+        ArrayAdapter<String> adapter;
+
+        adapter = new ArrayAdapter<String>
+                (activity, android.R.layout.simple_list_item_1, listNomProjets);
+
+        list_projets.setAdapter(adapter);
+
     }
 
     private List<File> getProjets() {
-        ArrayList<File> projets = new ArrayList<File>();
+        ArrayList<File> listProjets = new ArrayList<File>();
 
+        // Racine de l'application
         File racineApp = activity.getFilesDir();
-        Log.i("MyApp","Racine de l'application : " + racineApp);
 
-        // Dossier contenant les fichiers textes
-        File dossierApp = new File(racineApp.getAbsolutePath() + File.separator);
-        Log.i("MyApp","Dossier Texte : " + dossierApp);
+        // Dossier contenant les projets
+        File dossierApp = new File(racineApp.getAbsolutePath() + File.separator + varTempoNomUser);
 
+        // Liste des fichiers du répertoire
+        File[] files = dossierApp.listFiles();
 
-        return projets;
+        // Parcours des fichiers :
+        for (File file : files) {
+            // Si c'est un répertoire
+            if (file.isDirectory()) {
+                listProjets.add(file);
+            }
+
+            System.out.println("---------------------" + file.getName());
+
+        }
+        return listProjets;
     }
 
     @Override
