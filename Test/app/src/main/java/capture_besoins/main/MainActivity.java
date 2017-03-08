55b2cff47e1c6b3a5fd78d896fe9c76858c03dca
Karl Ferrero <karl.ferrero.kf@gmail.com>
2017-03-08 11:19:52 +0100

    #12 L'utilisateur peut se connecter avec son compte Google

--- Test/app/src/main/java/capture_besoins/main/MainActivity.java
+++ Test/app/src/main/java/capture_besoins/main/MainActivity.java
@@ -1,16 +1,30 @@
 package capture_besoins.main;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import capture_besoins.plugins.texte_simple.Plugin_texte_simple;
 import capture_besoins.projet.GestionProjets;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener{
 
-    Plugin_texte_simple pluginTexteSimple;
+    private static final int RC_SIGN_IN = 10;
 
     GestionProjets gestionProjet;
+
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +33,77 @@ public class MainActivity extends AppCompatActivity {
         // Layout associé à cette Activity
         setContentView(R.layout.activity_gestion_projets);
 
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        // Set the dimensions of the sign-in button.
+        SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+        // Récupération du bouton pour se connecter
+        findViewById(R.id.sign_in_button).setOnClickListener(this);
+
+        // Lancement de l'Activité de gestion des projets
         gestionProjet = new GestionProjets(this);
 
+
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        // TODO Gérer connection failed
+    }
+
+    private void signIn() {
+        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.sign_in_button:
+                signIn();
+                break;
+            // ...
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        //Log.d(TAG, "handleSignInResult:" + result.isSuccess());
+        if (result.isSuccess()) {
+            // Signed in successfully, show authenticated UI.
+            GoogleSignInAccount acct = result.getSignInAccount();
+
+            SignInButton signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+            System.out.println("CONNEXION : "+acct.getDisplayName());
+            //updateUI(true);
+        } else {
+            System.out.println("FAIL !!!");
+            // Signed out, show unauthenticated UI.
+            //updateUI(false);
+        }
     }
 }
