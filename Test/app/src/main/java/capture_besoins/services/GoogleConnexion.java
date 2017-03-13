140ae035451266e687a722aa9eae19125a55af95
Karl Ferrero <karl.ferrero.kf@gmail.com>
2017-03-13 14:57:33 +0100

    #12 On peut se connecter à Drive + test de création d'un fichier et accèes au Drive

--- Test/app/src/main/java/capture_besoins/services/GoogleConnexion.java
+++ Test/app/src/main/java/capture_besoins/services/GoogleConnexion.java
@@ -1,123 +1,311 @@
 package capture_besoins.services;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.net.Uri;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.drive.DriveApi.DriveContentsResult;
+import com.google.android.gms.drive.DriveContents;
+import com.google.android.gms.drive.DriveFile;
+import com.google.android.gms.drive.DriveFolder;
+import com.google.android.gms.drive.DriveId;
+import com.google.android.gms.drive.MetadataChangeSet;
+import com.google.android.gms.drive.OpenFileActivityBuilder;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 import capture_besoins.main.R;
+import capture_besoins.plugins.texte_word.Plugin_texte_word;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by Karl on 12/03/2017.
  */
 
-public class GoogleConnexion extends Fragment implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
+public class GoogleConnexion extends Fragment implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
 
-    private static final int RC_SIGN_IN = 10;
-
+    private static final String TAG = "Google Drive Activity";
+    private static final int REQUEST_CODE_RESOLUTION = 1;
+    private static final int REQUEST_CODE_OPENER = 2;
     private GoogleApiClient mGoogleApiClient;
-
-    private Activity activity;
+    private boolean fileOperation = false;
+    private DriveId mFileId;
+    public DriveFile file;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
-
-        // Build a GoogleApiClient with access to the Google Sign-In API and the
-        // options specified by gso.
-        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
-                .enableAutoManage((AppCompatActivity) getActivity() /* FragmentActivity */, this /* OnConnectionFailedListener */)
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build();
-
-
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.google_connexion, container, false);
     }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public void onResume() {
+        super.onResume();
+        if (mGoogleApiClient == null) {
 
-        // Bouton "Se connecter" de Google
-        SignInButton signInButton = (SignInButton) getActivity().findViewById(R.id.sign_in_button);
-        //signInButton.setSize(SignInButton.SIZE_WIDE);
+            getActivity().findViewById(R.id.createfile).setOnClickListener(this);
+            getActivity().findViewById(R.id.openfile).setOnClickListener(this);
+            /**
+             * Create the API client and bind it to an instance variable.
+             * We use this instance as the callback for connection and connection failures.
+             * Since no account name is passed, the user is prompted to choose.
+             */
+            mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
+                    .addApi(Drive.API)
+                    .addScope(Drive.SCOPE_FILE)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
 
-        // Ajout du listener
-        getActivity().findViewById(R.id.sign_in_button).setOnClickListener(this);
+        mGoogleApiClient.connect();
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mGoogleApiClient != null) {
+
+            // disconnect Google API client connection
+            mGoogleApiClient.disconnect();
+        }
+        super.onPause();
     }
 
     @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        // TODO Gérer connection failed
+    public void onConnectionFailed(ConnectionResult result) {
+
+        // Called whenever the API client fails to connect.
+        Log.i(TAG, "GoogleApiClient connection failed: " + result.toString());
+
+        if (!result.hasResolution()) {
+
+            // show the localized error dialog.
+            GoogleApiAvailability.getInstance().getErrorDialog(getActivity(), result.getErrorCode(), 0).show();
+            return;
+        }
+
+        /**
+         *  The failure has a resolution. Resolve it.
+         *  Called typically when the app is not yet authorized, and an  authorization
+         *  dialog is displayed to the user.
+         */
+
+        try {
+
+            result.startResolutionForResult(getActivity(), REQUEST_CODE_RESOLUTION);
+
+        } catch (IntentSender.SendIntentException e) {
+
+            Log.e(TAG, ";Exception while starting resolution activity", e);
+        }
     }
 
-    private void signIn() {
-        Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+    /**
+     * It invoked when Google API client connected
+     *
+     * @param connectionHint
+     */
+    @Override
+    public void onConnected(Bundle connectionHint) {
+
+        Toast.makeText(getActivity(), "Connected", Toast.LENGTH_LONG).show();
+    }
+
+    /**
+     * It invoked when connection suspended
+     *
+     * @param cause
+     */
+    @Override
+    public void onConnectionSuspended(int cause) {
+
+        Log.i(TAG, "GoogleApiClient connection suspended");
+    }
+
+    public void onClickCreateFile(View view) {
+        fileOperation = true;
+
+        // create new contents resource
+        Drive.DriveApi.newDriveContents(mGoogleApiClient)
+                .setResultCallback(driveContentsCallback);
+
+    }
+
+    public void onClickOpenFile(View view) {
+        fileOperation = false;
+
+        // create new contents resource
+        Drive.DriveApi.newDriveContents(mGoogleApiClient)
+                .setResultCallback(driveContentsCallback);
+    }
+
+    /**
+     * Open list of folder and file of the Google Drive
+     */
+    public void OpenFileFromGoogleDrive() {
+
+        IntentSender intentSender = Drive.DriveApi
+                .newOpenFileActivityBuilder()
+                .setMimeType(new String[]{"text/plain", "text/html"})
+                .build(mGoogleApiClient);
+        try {
+            getActivity().startIntentSenderForResult(intentSender, REQUEST_CODE_OPENER, null, 0, 0, 0);
+
+        } catch (IntentSender.SendIntentException e) {
+
+            Log.w(TAG, "Unable to send intent", e);
+        }
+
+    }
+
+    /**
+     * This is Result result handler of Drive contents.
+     * this callback method call CreateFileOnGoogleDrive() method
+     * and also call OpenFileFromGoogleDrive() method,
+     * send intent onActivityResult() method to handle result.
+     */
+    final ResultCallback<DriveContentsResult> driveContentsCallback =
+            new ResultCallback<DriveContentsResult>() {
+                @Override
+                public void onResult(DriveContentsResult result) {
+
+                    if (result.getStatus().isSuccess()) {
+
+                        if (fileOperation == true) {
+
+                            CreateFileOnGoogleDrive(result);
+
+                        } else {
+
+                            OpenFileFromGoogleDrive();
+
+                        }
+                    }
+
+                }
+            };
+
+    /**
+     * Create a file in root folder using MetadataChangeSet object.
+     *
+     * @param result
+     */
+    public void CreateFileOnGoogleDrive(DriveContentsResult result) {
+
+        final DriveContents driveContents = result.getDriveContents();
+
+        // Perform I/O off the UI thread.
+        new Thread() {
+            @Override
+            public void run() {
+                // write content to DriveContents
+                OutputStream outputStream = driveContents.getOutputStream();
+                Writer writer = new OutputStreamWriter(outputStream);
+                try {
+                    writer.write("Hello abhay!");
+                    writer.close();
+                } catch (IOException e) {
+                    Log.e(TAG, e.getMessage());
+                }
+
+                MetadataChangeSet changeSet = new MetadataChangeSet.Builder()
+                        .setTitle("abhaytest2")
+                        .setMimeType("text/plain")
+                        .setStarred(true).build();
+
+                // create a file in root folder
+                Drive.DriveApi.getRootFolder(mGoogleApiClient)
+                        .createFile(mGoogleApiClient, changeSet, driveContents)
+                        .setResultCallback(fileCallback);
+            }
+        }.start();
+    }
+
+    /**
+     * Handle result of Created file
+     */
+    final private ResultCallback<DriveFolder.DriveFileResult> fileCallback = new
+            ResultCallback<DriveFolder.DriveFileResult>() {
+                @Override
+                public void onResult(DriveFolder.DriveFileResult result) {
+                    if (result.getStatus().isSuccess()) {
+
+                        Toast.makeText(getActivity(), "file created: " + "" +
+                                result.getDriveFile().getDriveId(), Toast.LENGTH_LONG).show();
+
+                    }
+
+                    return;
+
+                }
+            };
+
+    /**
+     * Handle Response of selected file
+     *
+     * @param requestCode
+     * @param resultCode
+     * @param data
+     */
+    @Override
+    public void onActivityResult(final int requestCode,
+                                 final int resultCode, final Intent data) {
+        switch (requestCode) {
+
+            case REQUEST_CODE_OPENER:
+
+                if (resultCode == RESULT_OK) {
+
+                    mFileId = (DriveId) data.getParcelableExtra(
+                            OpenFileActivityBuilder.EXTRA_RESPONSE_DRIVE_ID);
+
+                    Log.e("file id", mFileId.getResourceId() + "");
+
+                    String url = "https://drive.google.com/open?id=" + mFileId.getResourceId();
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                }
+
+                break;
+
+            default:
+                super.onActivityResult(requestCode, resultCode, data);
+                break;
+        }
     }
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.sign_in_button:
-                signIn();
-                break;
-            // ...
+        // Si on a cliqué sur le bouton "Word"
+        if (view.getId() == R.id.createfile) {
+            onClickCreateFile(view);
         }
-    }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-            handleSignInResult(result);
-        }
-    }
-
-    private void handleSignInResult(GoogleSignInResult result) {
-        //Log.d(TAG, "handleSignInResult:" + result.isSuccess());
-        if (result.isSuccess()) {
-            // Signed in successfully, show authenticated UI.
-            GoogleSignInAccount acct = result.getSignInAccount();
-
-            SignInButton signInButton = (SignInButton) getActivity().findViewById(R.id.sign_in_button);
-
-            // Zone de texte
-            TextView myAwesomeTextView = (TextView) getActivity().findViewById(R.id.nom_google);
-
-            // Modification avec le nom de la personne connectée
-            myAwesomeTextView.setText(acct.getDisplayName());
-
-            //updateUI(true);
-        } else {
-            System.out.println("FAIL !!!");
-            // Signed out, show unauthenticated UI.
-            //updateUI(false);
+        if (view.getId() == R.id.openfile) {
+            // Intent pour switch entre 2 activities
+            onClickOpenFile(view);
         }
     }
 }
